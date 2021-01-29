fc48ac819e92f817ed51d29d3b411c10f081485e
aaoshchepkov <aa_oshchepkov@mail.ru>
2021-01-29 13:20:18 +0700

    Усложененный урок

---
+++ script.js
@@ -0,0 +1,8 @@
+let num = 266219;
+
+num = num.toString();
+console.log(num.charAt(0) * num.charAt(1) * num.charAt(2) * num.charAt(3) * num.charAt(4) * num.charAt(5));
+myNum = num.charAt(0) * num.charAt(1) * num.charAt(2) * num.charAt(3) * num.charAt(4) * num.charAt(5);
+myNum = myNum ** 3;
+myNum = myNum.toString();
+console.log(myNum.charAt(0) + myNum.charAt(1));
