4c3289ce3b88b3985735b33ec292fb44bd692811
aaoshchepkov <aa_oshchepkov@mail.ru>
2021-02-21 18:46:21 +0700

    усложненное 5

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-let num = 100;
 
+'use strict';
+
+let arr = ['654545646', '274654646', '75645644555', '1564654', '354564546', '75465465465', '37457']; 
+arr.forEach((item) => {
+  if (item.startsWith('3') || item.startsWith('7')){
+    console.log(item);
+  }
+});
+
+
+let num = 100;
 nextNum:
 for (let i = 2; i <= num; i++) {
   for (let j = 2; j < i; j++) { 
