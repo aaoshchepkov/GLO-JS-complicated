6693d9c1cba976603284e4300c4cac808830a65f
aaoshchepkov <aa_oshchepkov@mail.ru>
2021-02-21 21:36:51 +0700

    усложненное 11

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
 
 'use strict';
 
-let arr = ['654545646', '274654646', '75645644555', '1564654', '354564546', '75465465465', '37457']; 
-arr.forEach((item) => {
-  if (item.startsWith('3') || item.startsWith('7')){
-    console.log(item);
-  }
+let button = document.querySelector('.change');
+let title = document.querySelector('.color');
+let div = document.querySelector('.wrapper');
+
+
+let num;
+button.addEventListener('click',()=> {
+  num = Math.floor(Math.random()*16777215).toString(16);
+  div.style.backgroundColor = "#" + num;
+  title.textContent = "#" + num;
 });
-
-
-let num = 100;
-nextNum:
-for (let i = 2; i <= num; i++) {
-  for (let j = 2; j < i; j++) { 
-    if (i % j === 0) {
-    continue nextNum;
-  } 
-  }
- console.log('Простое число: ' + i + ', первый делитель 1,' +' второй делитель: ' + i); 
-}
