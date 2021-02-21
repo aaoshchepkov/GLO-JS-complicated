99f09fa98bf65ec7bb0afddf5b5ab558806aac00
aaoshchepkov <aa_oshchepkov@mail.ru>
2021-02-21 21:40:09 +0700

    усложенное 13

--- script.js
+++ script.js
@@ -4,9 +4,8 @@
 let button = document.querySelector('.change');
 let title = document.querySelector('.color');
 let div = document.querySelector('.wrapper');
-
-
 let num;
+
 button.addEventListener('click',()=> {
   num = Math.floor(Math.random()*16777215).toString(16);
   div.style.backgroundColor = "#" + num;
